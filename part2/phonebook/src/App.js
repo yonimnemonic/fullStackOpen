12f8eaec849ce432d0ae868814938c5c4ef6acb5
Agustin Martinez <conan93@gmail.com>
2022-01-24 21:53:29 +0100

    Set filter configured

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,55 +3,81 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Arto Hlas' },
-    { name: 'Arto las' },
-    { name: 'Art Hellas' },
-    { name: 'Artllas' },
+    { name: 'Arto Hellas', number: '6142521152' },
+    { name: 'Arturo Reverte', number: '62521152' },
+    { name: 'Meri Popins', number: '6122548452' },
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState();
+  const [ filter, setFilter ] = useState([]);
 
   const handleNewPerson = (event)=>{
         
      setNewName(event.target.value) 
   
   }
+  const handleNewNumber = (event)=>{
+        
+     setNewNumber(event.target.value) 
+  
+  }
 
-  const AddUser = (event)=>{
+  const PersonForm = (event)=>{
 
     event.preventDefault()
     
-    for(let name of persons){
+    for(let user of persons){
       
-      if(name.name.includes(newName)){
-        alert(`${newName} is already added to phonebook`)
+      if(user.name.includes(newName) || user.number.includes(newNumber)){
+        alert(`${newName}${newNumber} is already added to phonebook`)
         break
       }else{
         const addPerson = {
           name: newName,
+          number: newNumber,
         }
         setPersons(persons.concat(addPerson))
+
         
        }
     }
+  }
+  
+ const Filter = (event) => {
+  
+  //
+   setFilter(event.target.value);
+  
+   const regExpresion = new RegExp(filter, 'i')
+   const personFiltered = () => persons.filter( person => person.name.match(regExpresion));
 
+   if(regExpresion.length === 0){
+     setPersons()
+   }else{
+     setPersons(personFiltered);
+
+   }
+
+   console.log(filter)  
+       
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input onChange={handleNewPerson} />
-        </div>
-        <div>
-          <button type="submit" onClick={AddUser}>add</button>
-        </div>
+        filter shown with <input onChange={Filter}></input>
+        <p>We find: </p>
+      <form onSubmit={PersonForm}>
+          <h2>Add a New</h2>
+          name: <input onChange={handleNewPerson} /><br/>
+          number: <input onChange={handleNewNumber}/>
+          <button>add</button>
       </form>
       <h2>Numbers</h2>
-      <p>{persons.map((name, i) => <li key={i}>{name.name}</li>)}<br></br></p>
+      <p>{persons.map((name, i) => <li key={i}>{name.name}: {name.number}</li>)}<br></br></p>
         {/* <AddUser onClick={event} persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName}/> */}
-      <div>debug: {newName}</div>
+      <div>debugName: {newName}</div>
+      <div>debugNumber: {newNumber}</div>
       ...
     </div>
   )
