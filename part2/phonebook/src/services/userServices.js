830c6321d489aef95873c8c389e08c4dab4d463f
Agustin Martinez <conan93@gmail.com>
2022-07-10 11:38:48 +0200

    Fix heroku

--- part2/phonebook/src/services/userServices.js
+++ part2/phonebook/src/services/userServices.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-
-// const baseURL = 'http://localhost:3001/api/persons';
- const baseURL = '/api/persons';
+const baseURL = '/api/persons';
 
 
 export const getUser = () => {
