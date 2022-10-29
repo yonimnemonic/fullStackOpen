f0023ed27e4a69cf6313c8f2d343233e3d80f9d3
Agustín Martínez Bergillo <conan93@gmail.com>
2022-10-29 22:12:52 +0200

    Finish exercice 4.9

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,21 +2,17 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogsRouter.get('/', (request, response, next) => {
-    Blog
-    .find({})
-    .then(blogs => { 
-      response.json(blogs)
-    })
+blogsRouter.get('/', async(request, response, next) =>{
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async(request, response, next) =>{
+    const blog = await new Blog(request.body)
+    blog.save()
+    response.status(201).json(result)
+   
 })
+
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const blog = require('../models/blog')
+const { deleteOne } = require('../models/blog')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('Fetching existing blogs in json format' , ()=>{
+  test('blogs are returned as json', async ()=> {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+test('Checks that _id is the default identifier', async()=>{
+  const response = await api.get('/api/blogs')
+  const result = response.body.map(resp => resp.id)
+  
+  expect(result).toBeDefined()
+  
 })
 
 afterAll(() => {
